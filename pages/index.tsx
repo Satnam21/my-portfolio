bce414d53d81b0ebe682f53a249b847f54c1d969
Satnam Singh <satnamsingh23141@gmail.com>
2025-02-01 00:11:10 +0530

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import AboutMe from '@/components/AboutMe';
-import Blog from '@/components/Blog';
+// import Blog from '@/components/Blog';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Nav from '@/components/Nav'
 import NavMobile from '@/components/NavMobile'
-import Price from '@/components/Price';
+// import Price from '@/components/Price';
 import Project from '@/components/Project';
-import Reviews from '@/components/Reviews';
+// import Reviews from '@/components/Reviews';
 import Services from '@/components/Services';
 import Skills from '@/components/Skills';
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
-import SingleBlog from '@/components/SingleBlog';
+// import SingleBlog from '@/components/SingleBlog';
 
 const HomePage = () => {
   const [showNav, setShowNav] = useState(false);
